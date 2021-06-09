ab95389ed3902977c5c3e91ddd24a7b20ad89a71
Kvothe <criptoeconomiaorg@protonmail.com>
2021-06-09 03:57:18 -0500

    Updates

---
+++ precache-manifest.56dc8b1678b456521dab301ab8dd5bfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1a734507739680f5b45c36c63fcc95",
+    "url": "/nft_factory/index.html"
+  },
+  {
+    "revision": "eac3d47cbd54fa7025e4",
+    "url": "/nft_factory/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "3c77446789d866cf411e",
+    "url": "/nft_factory/static/css/main.1093c03b.chunk.css"
+  },
+  {
+    "revision": "eac3d47cbd54fa7025e4",
+    "url": "/nft_factory/static/js/2.e6b80535.chunk.js"
+  },
+  {
+    "revision": "3c77446789d866cf411e",
+    "url": "/nft_factory/static/js/main.170f5335.chunk.js"
+  },
+  {
+    "revision": "dc1405043fa9737a9e97",
+    "url": "/nft_factory/static/js/runtime-main.f5bf7a73.js"
+  }
+]);
