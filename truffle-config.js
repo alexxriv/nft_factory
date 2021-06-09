bdb9aaa894a150b98780355266e057c1e68dd7ad
Kvothe <criptoeconomiaorg@protonmail.com>
2021-06-09 04:13:52 -0500

    Added .gitignore file

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,74 @@
 const path = require("path");
+require('dotenv').config(path);
 
+const HDWalletProvider  = require('@truffle/hdwallet-provider');
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  
   networks: {
     develop: {
-      port: 7545
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+
+
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://mainnet.infura.io/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      confirmations: 2,
+      network_id: 1
+    },
+    
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3
+    },
+
+    binance: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          'https://bsc-dataseed.binance.org'
+        )
+      },
+      gas: 5000000,
+      network_id: 56
+    },
+
+    binanceTestnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          'https://data-seed-prebsc-1-s1.binance.org:8545/'
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 97
     }
   },
-  compilers: {
-    solc: {
-      optimizer:{
-        enabled: true,
-        runs: 200
-      },
-      version: "0.8.0"
+
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
+
 };
